854d1f446cab9013d9c6d61b6c8ca33925448c0e
BaduA <batualpustaguel@gmail.com>
2025-01-31 13:47:49 +0300

    Client Brand

--- client/app/(auth)/index.tsx
+++ client/app/(auth)/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import Brand from '@/components/auth/brand'
 
 const AuthIndex = () => {
   return (
     <View>
+      <Brand></Brand>
       <Text>SIGN IN</Text>
     </View>
   )
---
+++ client/components/auth/brand.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Brand = () => {
+  return (
+    <View style={{width:"100%", height:90, backgroundColor:"#E9DFC4",justifyContent:"center", alignItems:"center",paddingTop:45}}>
+      <Text>Brand</Text>
+    </View>
+  )
+}
+
+export default Brand
