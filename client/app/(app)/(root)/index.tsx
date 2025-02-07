7c2a28000bb45420b2ae90f5010d69cc041056c8
BaduA <batualpustaguel@gmail.com>
2025-02-07 13:59:04 +0300

    index page

--- client/app/(app)/(root)/index.tsx
+++ client/app/(app)/(root)/index.tsx
@@ -1,26 +1,75 @@
-import { View, Text, Pressable, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, ScrollView, TouchableOpacity, TextInput } from 'react-native'
+import React, { useState } from 'react'
 import { router } from 'expo-router'
 import { useDispatch } from 'react-redux'
 import { logout } from '@/redux/slices/authSlice'
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { ImageBackground } from 'expo-image'
+import { LinearGradient } from 'expo-linear-gradient'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { BlurView } from 'expo-blur'
+import Loading from '@/components/Loading'
 
 const Home = () => {
+  const [search, setSearch] = useState("")
+  const [imageLoad, setImageLoad] = useState(false)
   const dispatch = useDispatch()
+  console.log("xx")
+  const readList = [{ name: "Makale 1", image: require("../../../assets/images/4.png") }, { name: "Makale 1", image: require("../../../assets/images/4.png") }]
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ marginBottom:20, flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-        <FontAwesome5 name="leaf" size={40} color="rgb(0, 69, 26)" />
-        <Text style={{ marginLeft: 20, fontSize: 40, fontWeight: "bold", color: "rgb(0, 69, 26)" }}>Ecopmo</Text>
+      <ImageBackground style={{ flex: 1 }} source={require("../../../assets/ecopmowallpaper.png")} onLoad={() => { setImageLoad(true) }}>
+        <View style={{ marginBottom: 20, flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+          <FontAwesome5 name="leaf" size={40} color="rgb(0, 69, 26)" />
+          <Text style={{ marginLeft: 20, fontSize: 40, fontWeight: "bold", color: "rgb(0, 69, 26)" }}>Ecopmo</Text>
+        </View>
+      </ImageBackground>
+      <View style={{
+        backgroundColor: "rgba(210, 210, 210, 0.95)",
+        borderRadius: 20,
+        flex: 1,
+        width: "100%",
+        aspectRatio: 1 / 1.3,
+        position: "absolute",
+        bottom: 0
+      }}>
+        <ScrollView style={{ padding: 20 }}>
+          <View>
+            <View style={{
+              width: "100%",
+              height: 40,
+              backgroundColor: "rgba(144, 144, 144, 0.4)",
+              borderRadius: 50,
+              alignItems: "center",
+              flexDirection: "row",
+              paddingLeft: 20,
+              marginBottom: 20
+            }}>
+              <FontAwesome name="search" size={18} color="rgba(110, 110, 110, 0.83)" />
+              <TextInput style={{ width: "100%", marginLeft: 10 }} placeholder='Search for places...' />
+
+            </View>
+          </View>
+          <View>
+            <Text style={{ marginBottom: 10, fontSize: 22, fontWeight: "600" }}>Oku</Text>
+            <ScrollView horizontal>
+              {readList.map((element, index) =>
+                <TouchableOpacity style={{ marginRight: 10 }} key={index}>
+                  <ImageBackground
+                    source={element.image}
+                    style={{ width: 300, height: 180, borderRadius: 20, overflow: "hidden" }}>
+                    <LinearGradient colors={["rgba(5, 5, 5, 0)", "rgb(0, 27, 3)"]} style={{ marginTop: "auto", width: "100%", height: "30%" }}>
+
+                    </LinearGradient>
+
+
+                  </ImageBackground>
+                </TouchableOpacity>)}
+            </ScrollView>
+          </View>
+        </ScrollView>
       </View>
-      <ScrollView>
-        <Pressable style={{ width: "30%", height: 50, backgroundColor: "red" }} onPress={() => {
-          dispatch(logout())
-        }}><Text>SignOut</Text></Pressable>
-        <Pressable style={{ width: "30%", height: 50, backgroundColor: "red" }} onPress={() => {
-          router.push("/(app)/(root)/contributionToNature")
-        }}><Text>To Contribution to nature</Text></Pressable>
-      </ScrollView>
+      <Loading isShown={!imageLoad} intensity={200}/>
     </View>
   )
 }
--- client/components/Loading.tsx
+++ client/components/Loading.tsx
@@ -8,11 +8,12 @@ interface LoadingProps {
     isShown: boolean,
     blurHash?: string,
     children?: ReactNode
+    intensity?: number
 }
-const Loading = ({ isShown, blurHash, children }: LoadingProps): JSX.Element => {
+const Loading = ({ isShown, blurHash, children, intensity }: LoadingProps): JSX.Element => {
     return (
         <Modal transparent={true} animationType="fade" visible={isShown}>
-            <BlurView style={{ flex: 1, justifyContent: "center", alignItems: "center" }} intensity={60}>
+            <BlurView style={{ flex: 1, justifyContent: "center", alignItems: "center" }} intensity={intensity || 60}>
                 {blurHash ? <ImageBackground placeholder={{ blurHash }} style={{ flex: 1 }}><ActivityIndicator size="large" /></ImageBackground> : <ActivityIndicator size="large" />}
             </BlurView>
         </Modal>
